cab202f8a25db395f099ab78e23de7ca48e33b21
Simone Ergotino <simoneergotino@gmail.com>
2019-07-23 10:48:47 +0200

    ic-datepicker-agena init

--- src/interfaces/ic-datepicker-quick-option.ts
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-import { IcDatepickerDay } from './ic-datepicker-day';
-
-export interface IcDatepickerQuickOption {
-  date: moment.Moment;
-  datepickerDay?: IcDatepickerDay;
-  isDisabled?: boolean;
-  label: string;
-}
--- src/interfaces/ic-datepicker-year.ts
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-
-export interface IcDatepickerYear {
-  formatted: string | null;
-  isDisabled: boolean;
-  isSelected: boolean;
-  isThisYear: boolean;
-  moment: moment.Moment;
-}
